634b7059c186fb016b809cf501d6f419dc7c8701
Marcos Vinicius <marcos.viniciuscruz00@gmail.com>
2023-08-17 23:21:10 -0300

    implementando DepartmentDao e testando

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,16 @@
 package model.dao;
 
 import db.DB;
+import model.model.dao.impl.DepartmentDaoJDBC;
 import model.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao creatSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao creatDepartmentDao(){
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
